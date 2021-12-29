bd58edcdf39e470bce53d13b48663f2156f2d4f4
Prateek Niket <prateekniket25@gmail.com>
2021-12-29 15:29:34 +0530

    added svg logo

---
+++ src/components/homeLoadingSection/HomeLoadingSection.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './HomeLoadingSection.css'
+
+const HomeLoadingSection = () => {
+    return (
+    <div className='svg'>
+        <svg width="227" height="263" viewBox="0 0 227 263" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M116.5 63.5L73 88L116.5 113.5V63.5Z" stroke="#2D9586" stroke-width="3"/>
+            <path d="M116.5 113.5L157 87.5L116.5 63.5V113.5Z" stroke="#2D9586" stroke-width="3"/>
+            <path d="M116.5 160L157 137.5L116.5 113.5V160Z" stroke="#2D9586" stroke-width="3"/>
+            <path d="M157 137.5L117 113.5L157 88V137.5Z" stroke="#2D9586" stroke-width="3"/>
+            <path d="M116.5 113.5L73.5 88.5V184L116.5 207V113.5Z" stroke="#2D9586" stroke-width="3"/>
+            <path d="M2 66.5L113.5 2" stroke="#2D9586" stroke-width="3"/>
+            <path d="M1.5 197V66" stroke="#2D9586" stroke-width="3"/>
+            <path d="M0.999998 196L113.5 261" stroke="#2D9586" stroke-width="3"/>
+            <path d="M113 2L225.5 67" stroke="#2D9586" stroke-width="3"/>
+            <path d="M113 261.5L224.5 197" stroke="#2D9586" stroke-width="3"/>
+            <path d="M225 197V66" stroke="#2D9586" stroke-width="3"/>
+        </svg>
+    </div>
+    )
+}
+
+export default HomeLoadingSection
